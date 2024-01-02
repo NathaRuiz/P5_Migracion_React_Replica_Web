854b529db8c9d1460e4361b8106ccd9278a1f630
NathaRuiz <nruizm98@gmail.com>
2024-01-02 16:48:01 +0100

    create the component userTestimonial

---
+++ src/components/Cards/UserTestimonial.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UserTestimonial = ({opinion, name, profession}) => {
+  return (
+    <>
+      <div className="user text-center flex flex-col items-center justify-evenly w-[32%]">
+        <div className="starts text-comments-icon">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+        </div>
+        <div className="border w-10 h-[1.7px] mt-[5%] mb-[5%] bg-line-color"></div>
+        <p className="opinion text-secondary-letter-color leading-[1.4em] italic">{opinion}</p>
+        <p className="name font-bold text-primary-letter-color mt-[3%] not-italic">{name} - {profession}</p>
+      </div>
+    </>
+  );
+};
+
+export default UserTestimonial;
--- src/views/Screens.jsx
+++ src/views/Screens.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import UserTestimonial from '../components/Cards/UserTestimonial'
 
 const Screens = () => {
   return (
-    <div>Screens</div>
+    <>
+      <article className='user-testimonials'>
+
+        <div className='section-title'>
+          <h2>User Testimonials</h2>
+        </div>
+
+        <section className='testimonials'>
+          <div className='users'>
+            <UserTestimonial opinion={"I love using Sync for my personal development needs. It meets all my requirements and it actually helps a lot with focusing skills."}
+                             name={"Rick Jones "} profession={" Designer"}/>
+          </div>
+
+        </section>
+
+      </article>
+    </>
   )
 }
 
